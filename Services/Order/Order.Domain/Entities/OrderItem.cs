a181ee731afe318f34345a639e647bc6e3ab41a6
Abenezer Bayu <abayu@excellerentsolutions.com>
2021-08-11 14:05:31 +0300

    Add project files.

---
+++ Services/Order/Order.Domain/Entities/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using Order.Domain.Models;
+using Order.Domain.Seed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Domain.Entities
+{
+    public class OrderItem : BaseEntity<OrderItemModel>
+    {
+        public decimal Qty { get; protected set; }
+        public string Name { get; protected set; }
+        public decimal Price { get; protected set; }
+
+        public override OrderItemModel MapToModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override OrderItemModel MapToModel(OrderItemModel t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Order/Order.Domain/Interfaces/Facade/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Domain.Interfaces.Facade
+{
+    public interface IOrderService
+    {
+        Task<List<Entities.Order>> GetAllOrders();
+        Task<Guid> Add(Entities.Order order);
+        Task<List<Entities.Order>> GetByUser(string user);
+        Task<Entities.Order> GetOrder(Guid id);
+        Task UpdateOrder(Entities.Order order);
+    }
+}
